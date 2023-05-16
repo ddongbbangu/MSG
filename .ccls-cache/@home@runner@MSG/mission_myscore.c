c8c112f6fdbcb8e6329bed99d58d6ca56cb89236
ddongbbangu <128569797+ddongbbangu@users.noreply.github.com>
2023-05-16 06:26:49 +0000

    조건문 끝

---
+++ .ccls-cache/@home@runner@MSG/mission_myscore.c
@@ -0,0 +1,21 @@
+// #include <stdio.h>
+// main(){
+//   int a;
+//   scanf("%d", &a);
+//   if(a>89){
+//     printf("A");
+//   }
+//   else if(a>79){
+//     printf("B");
+//   }
+//   else if(a>69){
+//     printf("C");
+//   }
+//   else if(a>59){
+//     printf("D");
+//   }
+//   else if(a>0){
+//     printf("F");
+//   }
+//   printf("시험보느라 수고하셨습니다!!");
+// }
---
+++ .ccls-cache/@home@runner@MSG/switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main(){
+  int a;
+ 
+  printf("1: 사과 | 2: 키위 | 3: 복숭아 | 4: 수박 | 0: 모두 좋아하지 않는다\n");
+  scanf("%d", &a);
+
+  switch(a){
+    case 1:
+      printf("사과를 좋아하는군요!\n"); 
+    break;
+    case 2:
+      printf("키위를 좋아하는군요!\n");
+    break;
+    case 3:
+      printf("복숭아를 좋아하는군요\n");
+    break;
+    case 4:
+      printf("수박을 좋아하는 군요!\n");
+    break;
+    default:
+      printf("당신은 아마도 과일을 좋아하지 않는 것 같군요!");
+  }
+  printf("좋아하는 과일조사 끝!!><");
+}
